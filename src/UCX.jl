c3ffc9556d9394a2b8f02481b114f5220e754de5
Valentin Churavy <v.churavy@gmail.com>
2021-01-31 20:58:46 -0500

    update to 1.9

--- src/UCX.jl
+++ src/UCX.jl
@@ -51,15 +51,6 @@ function version()
     VersionNumber(major[], minor[], patch[])
 end
 
-function __init__()
-    lib_ver = version()
-    api_ver = VersionNumber(API.UCP_API_MAJOR, 
-                            API.UCP_API_MINOR,
-                            lib_ver.patch)
-    # TODO: Support multiple library versions in one package.
-    @assert lib_ver === api_ver
-end
-
 mutable struct UCXConfig
     handle::Ptr{API.ucp_config_t}
 
