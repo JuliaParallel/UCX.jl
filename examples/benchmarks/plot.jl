70be4b328061ae07abf7e5b83ba295e3e4005e7b
Valentin Churavy <v.churavy@gmail.com>
2021-02-22 13:46:00 -0500

    fixup! fix UCX benchmarks

--- examples/benchmarks/plot.jl
+++ examples/benchmarks/plot.jl
@@ -5,19 +5,22 @@ using CSV
 # Latency
 
 dist_latency = Table(CSV.File("distributed/latency.csv"))
-ucx_latency = Table(CSV.File("ucx/latency.csv"))
+legacy_latency = Table(CSV.File("legacy/latency_tcp.csv"))
+ucx_latency = Table(CSV.File("ucx/latency_tcp.csv"))
 mpi_latency = Table(CSV.File("mpi/latency.csv"))
 
 let
     f = Figure()
-    fig = f[1, 1] = Axis(f, palette = (color = [:black],))
+    fig = f[1, 1] = Axis(f)
     fig.xlabel = "Message size (bytes)"
     fig.ylabel = "Latency (ns)"
 
-    lines!(dist_latency.msg_size, dist_latency.latency, label = "Distributed", linewidth = 2)
-    lines!(ucx_latency.msg_size, ucx_latency.latency, label = "UCX", linewidth = 2)
-    lines!(mpi_latency.msg_size, mpi_latency.latency, label = "MPI", linewidth = 2)
+    lines!(dist_latency.msg_size, dist_latency.latency, label = "Distributed", linewidth = 2, color=:red)
+    lines!(legacy_latency.msg_size, legacy_latency.latency, label = "Distributed over UCX", linewidth = 2, color=:blue)
+    lines!(ucx_latency.msg_size, ucx_latency.latency, label = "UCX", linewidth = 2, color=:green)
+    lines!(mpi_latency.msg_size, mpi_latency.latency, label = "MPI", linewidth = 2, color=:black)
 
     f[1, 2] = Legend(f, fig)
     f
+    save("latency.png", f)
 end
