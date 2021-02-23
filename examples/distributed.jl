387ebc5c63b5279cfec5d1b10d37e6bb8f5be191
Valentin Churavy <v.churavy@gmail.com>
2021-02-22 21:33:49 -0500

    send arguments early and without serialization if possible

--- examples/distributed.jl
+++ examples/distributed.jl
@@ -12,8 +12,12 @@ end
 @everywhere using UCX
 @everywhere UCX.Legacy.wireup()
 
-@test UCX.Legacy.remotecall_fetch(()->true, 2)
 @test fetch(UCX.Legacy.remotecall(()->true, 2))
+@test UCX.Legacy.remotecall_fetch(()->true, 2)
+
+data = rand(8192)
+UCX.Legacy.remotecall_wait(sum, 2, data) 
+@test UCX.Legacy.remotecall_fetch(sum, 2, data) == sum(data)
 
 
 @test UCX.Legacy.remotecall_fetch(()->true, 1)
